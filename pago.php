09f58f1ecb59a8642b1298561c55fe0dcf7677b8
JuanCarlos433 <95895601+JuanCarlos433@users.noreply.github.com>
2023-11-21 09:16:30 -0600

    funcionalidad de metodo de pago

--- pago.php
+++ pago.php
@@ -1,3 +1,23 @@
+<?php
+    session_start();
+    
+    if(!isset($_SESSION['logueado'])|| !$_SESSION['logueado']){
+        header('Location: login.html');
+    }
+    
+    require_once('assets/php/conexion.php');
+    mysqli_set_charset($conn,'utf8');
+    $usuario=$_SESSION['emailUs'];
+    if($_POST){
+        $active=$_POST['nombre'];
+        $sql="UPDATE usuarios SET Esta_Suscrito='1' WHERE Correo ='$usuario'";
+        $env=mysqli_query($conn,$sql);
+        echo '<script> alert("Su pago se realizo con exito") </script>';
+        header("Location: index.php");
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,31 +33,34 @@
                 <h1>Suscribete</h1>
                 <p>Tansolo paga $299.00 y haste vip ya</p>
                 <div class="input-box">
-                    <input type="text" required>
+                    <input type="text" required name='nombre'>
                     <label>Nombre del propietario</label>
                 </div>
                 <div class="input-box">
-                    <input type="number" required>
+                    <input type="number" required name='targeta'>
                     <label>Numero de targeta</label>
                 </div>
                 <div class="input-box">
-                    <input type="number" required>
+                    <input type="number" required name='CVV'>
                     <label>CVV</label>
                 </div>
                 <div class="input-box">
-                    <input type="number" required>
+                    <input type="number" required name='fecha'>
                     <label>Fecha de vencimiento</label>
                 </div>
                 <div class="input-box">
-                    <input type="number" required>
+                    <input type="number" required name='codigo'>
                     <label>Codigo postal</label>
                 </div>
                 <div class="input-box">
-                    <input type="text" required>
+                    <input type="text" required name='direccion'>
                     <label>Direccion</label>
                 </div>
                 <a href="#" class="c1">
                     <span id="span1"></span>
+                <div class='c1'>
+                    <span id="span1"></span>
+                    <input class='terminar' type='submit' value='paga ya'></input>
                     <span id="span2"></span>
                     <span id="span3"></span>
                     <span id="span4"></span>
