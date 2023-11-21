47d14d2f18f834a30cc762c0f88eb5aa40abb532
milosnowcat <ramiro.alvarez@rahcode.com>
2023-11-21 11:58:41 -0600

    only allow create artists to disquera and admin

--- assets/php/crear-artista.php
+++ assets/php/crear-artista.php
@@ -1,32 +1,49 @@
 <?php
-require_once 'conexion.php';
+session_start();
 
-$path = '/assets/img/artists/';
-
-$nombre = $_POST['nombre'];
-$descripcion = $_POST['descripcion'];
-$foto = $_FILES['foto'];
-$disquera = $_POST['disquera'];
-
-$sql_verificacion = "SELECT Id FROM artistas WHERE Nombre = '$nombre' AND Id_Disquera = $disquera";
-$resultado_verificacion = $conn->query($sql_verificacion);
-
-if ($resultado_verificacion->num_rows > 0) {
-    echo '<script>window.alert("Esta disquera ya tiene asignado a este artista.");
-    location.href="/crear-artista.php";</script>';
+if(!isset($_SESSION['id']) || $_SESSION['tipo'] < 2) {
+?>
+    <script>
+    alert("Inicia sesión como disquera para entrar");
+    location.href = "/login.html";
+    </script>
+<?php
 } else {
-    if (is_uploaded_file($foto['tmp_name'])) {
-        $foto_path = $path . $nombre . '.jpg';
-        move_uploaded_file($foto['tmp_name'], '../..' . $foto_path);
-        $sql = "INSERT INTO artistas (Nombre, Descripcion, Foto, Id_Disquera)
-        VALUES ('$nombre', '$descripcion', '$foto_path', $disquera)";
+    require_once 'conexion.php';
+
+    $path = '/assets/img/artists/';
+
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $foto = $_FILES['foto'];
+    
+    if ($_SESSION['tipo'] == 3) {
+        $disquera = $_POST['disquera'];
     } else {
-        $sql = "INSERT INTO artistas (Nombre, Descripcion, Id_Disquera) VALUES ('$nombre', '$descripcion', $disquera)";
+        $disquera = $_SESSION['id'];
     }
 
-    if ($conn->query($sql)) {
-        header("Location: /crear-artista.php");
+    $sql_verificacion = "SELECT Id FROM artistas WHERE Nombre = '$nombre' AND Id_Disquera = $disquera";
+    $resultado_verificacion = $conn->query($sql_verificacion);
+
+    if ($resultado_verificacion->num_rows > 0) {
+        echo '<script>window.alert("Esta disquera ya tiene asignado a este artista.");
+        location.href="/crear-artista.php";</script>';
     } else {
-        echo('Ha ocurrido un error al insertar los datos >:3 <img src="/assets/img/cat.jpeg" alt="gato">'); 
+        if (is_uploaded_file($foto['tmp_name'])) {
+            $foto_path = $path . $nombre . '.jpg';
+            move_uploaded_file($foto['tmp_name'], '../..' . $foto_path);
+            $sql = "INSERT INTO artistas (Nombre, Descripcion, Foto, Id_Disquera)
+            VALUES ('$nombre', '$descripcion', '$foto_path', $disquera)";
+        } else {
+            $sql = "INSERT INTO artistas (Nombre, Descripcion, Id_Disquera)
+                VALUES ('$nombre', '$descripcion', $disquera)";
+        }
+
+        if ($conn->query($sql)) {
+            header("Location: /crear-artista.php");
+        } else {
+            echo 'Ha ocurrido un error al insertar los datos >:3 <img src="/assets/img/cat.jpeg" alt="gato">';
+        }
     }
 }
--- crear-artista.php
+++ crear-artista.php
@@ -1,37 +1,54 @@
 <?php
-include_once 'assets/php/conexion.php';
-?>
+session_start();
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <form action="assets/php/crear-artista.php" method="post" enctype="multipart/form-data">
-      <label for="nombre">Nombre</label>
-      <input type="text" name="nombre" id="nombre" required />
-      <label for="descripcion">Descripción</label>
-      <textarea name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
-      <label for="foto">Foto</label>
-      <input type="file" name="foto" id="foto" accept="image/*" />
-      <img id="preview" />
-      <label for="disquera">Disquera</label>
-      <select name="disquera" id="disquera" required>
+if(!isset($_SESSION['id']) || $_SESSION['tipo'] < 2) {
+?>
+<script>
+  alert("Inicia sesión como disquera para entrar");
+  location.href = "/login.html";
+</script>
+<?php
+} else {
+  include_once 'assets/php/conexion.php';
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <form action="assets/php/crear-artista.php" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" required />
+        <label for="descripcion">Descripción</label>
+        <textarea name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
+        <label for="foto">Foto</label>
+        <input type="file" name="foto" id="foto" accept="image/*" />
+        <img id="preview" src="/assets/img/artist.jpg" />
         <?php
-        $query = 'SELECT Id, Nombre FROM usuarios WHERE Es_Disquera = 1';
-        $result = mysqli_query($conn, $query);
-        while ($row = mysqli_fetch_array($result)) {
-        ?>
-          <option value="<?php echo $row['Id'] ?>"><?php echo $row['Nombre'] ?></option>
+        if ($_SESSION['tipo'] == 3) {
+          ?>
+          <label for="disquera">Disquera</label>
+          <select name="disquera" id="disquera" required>
+            <?php
+            $query = 'SELECT Id, Nombre FROM usuarios WHERE Tipo_Usuario = 2';
+            $result = mysqli_query($conn, $query);
+            while ($row = mysqli_fetch_array($result)) {
+            ?>
+              <option value="<?php echo $row['Id'] ?>"><?php echo $row['Nombre'] ?></option>
+            <?php
+            }
+            ?>
+          </select>
         <?php
         }
         ?>
-      </select>
-      <input type="submit" value="Crear">
-    </form>
-    <script type="module" src="assets/js/crear-artista.js"></script>
-  </body>
-</html>
+        <input type="submit" value="Crear">
+      </form>
+      <script type="module" src="assets/js/crear-artista.js"></script>
+    </body>
+  </html>
+<?php
+  }
