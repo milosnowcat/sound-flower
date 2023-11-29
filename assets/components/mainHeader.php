3a204ce52b98905c58925e3c35625f5284956eae
DrakoZC <crisalisafe@gmail.com>
2023-11-28 21:39:32 -0600

    Corrección header y footer

--- assets/components/mainHeader.php
+++ assets/components/mainHeader.php
@@ -14,43 +14,33 @@
     }
     
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="assets/components/mainHeader.css">
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
-    <script src="assets/js/redirecciones.js"></script>
-</head>
-<body>
-    <header id="main_Header">
-        <section onclick="redireccionIndex()" id="logo_Container">
-            <img src="logo.svg">
-        </section>
+<link rel="stylesheet" href="assets/components/mainHeader.css">
+<link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+<script src="assets/js/redirecciones.js"></script>
 
-        <nav id="navegation_Conatiner">
-            <form action="" method="post">
-                <label>
-                    <section id="search_Container">
-                        <input type="search" 
-                        name="searchHeader"
-                        placeholder="¿Qué canción quieres escuchar?"
-                        >
-                    </section>
-                    <section id="lupa_Container">
-                        <i class='bx bx-search'></i>
-                    </section>
-                </label>
-            </form>
-        </nav>
+<header id="main_Header">
+    <section onclick="redireccionIndex()" id="logo_Container">
+        <img src="logo.svg">
+    </section>
 
-        <section onclick="redireccionUsuario(<?php echo $id ?>)" id="user_Container">
-            <img src="<?php echo $img ?>">                    
-        </section>
-    </header>
-</body>
-</html>
+    <nav id="navegation_Conatiner">
+        <form action="" method="post">
+            <label>
+                <section id="search_Container">
+                    <input type="search" 
+                    name="searchHeader"
+                    placeholder="¿Qué canción quieres escuchar?"
+                    >
+                </section>
+                <section id="lupa_Container">
+                    <i class='bx bx-search'></i>
+                </section>
+            </label>
+        </form>
+    </nav>
 
-
+    <section onclick="redireccionUsuario(<?php echo $id ?>)" id="user_Container">
+        <img src="<?php echo $img ?>">                    
+    </section>
+</header>
