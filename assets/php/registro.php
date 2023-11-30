eb3333650d095fde5759f099a47f1adb14eda5a5
DrakoZC <crisalisafe@gmail.com>
2023-11-30 13:02:33 -0600

    comentarios de las paginas no tan descriptivas al inicio

--- assets/php/registro.php
+++ assets/php/registro.php
@@ -10,35 +10,21 @@
     $verificarCorreo = "SELECT 'Id' FROM usuarios WHERE Correo = '$email'";
     $resultadoVC = mysqli_query($conn, $verificarCorreo);
 
-        if (mysqli_num_rows($resultadoVC) > 0){
-            echo'<script> alert("El correo que pusiste ya esta siendo usado.") </script>';
+    if (mysqli_num_rows($resultadoVC) > 0){
+        echo'<script> alert("El correo que pusiste ya esta siendo usado.") </script>';
         echo'<script> window.location.href = "/registro.html" </script>';
-            //aqui el mensajede que ya hay una cuenta con ese correo
-        }
-        elseif ($pass1 == $pass2){
-            $pass = password_hash($pass1, PASSWORD_DEFAULT, ['cost' => 15]);
+        //aqui el mensajede que ya hay una cuenta con ese correo
+    }
+    elseif ($pass1 == $pass2){
+        $pass = password_hash($pass1, PASSWORD_DEFAULT, ['cost' => 15]);
         $preparacion = "INSERT INTO usuarios (Nombre, Correo, Pass) VALUES ('$user', '$email', '$pass')";
             
         $envio = mysqli_query($conn, $preparacion);
         header('Location: /login.html');
-        }
-        else{
-            echo'<script> alert("Las contraseñas son diferentes.") </script>';
-            echo'<script> window.location.href = "/registro.html" </script>';
-            //aqui el mensaje de que las  contraseñas son distintas
     }
-
-
-
-
-    // $has=password_hash($contrasea,PASSWORD_DEFAULT,['cost'=>5]);
-    // if($contrasea==$confirme){
-    //     $sql="INSERT INTO usuarios(Nombre, Correo, Contraseña) VALUES ('$usuario', '$correo', '$has')";
-    //     $envio=mysqli_query($conexion, $sql);
-    //     header('location: login.html');
-    // }
-    // else{
-    //     echo '<script> alert("Error en tu confirmacion de contraseña") </script>';
-    //     echo'<script> window.location.href("registro.html") </script>';
-    // }
+    else{
+        echo'<script> alert("Las contraseñas son diferentes.") </script>';
+        echo'<script> window.location.href = "/registro.html" </script>';
+        //aqui el mensaje de que las  contraseñas son distintas
+    }
 ?>
